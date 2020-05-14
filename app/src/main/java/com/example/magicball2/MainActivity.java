b2c1f12c4c1b580608bca7d4baefa9bec5604f82
unknown <byron.carvajal@mail.udp.cl>
2020-05-13 22:02:44 -0400

    proyecto magic ball v2.0, proyecto programacion 1

--- app/src/main/java/com/example/magicball2/MainActivity.java
+++ app/src/main/java/com/example/magicball2/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.magicball2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ImageView ball;
+    private TextView respuesta;
+    private String [] arrayRespuesta = {"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
+            "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
+            "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
+            "Mi respuesta es no", "Mis fuentes dicen que no", "Muy dudoso"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ball = findViewById(R.id.boton);
+
+        respuesta = findViewById(R.id.respuesta);
+
+        ball.setOnClickListener((View.OnClickListener)this);
+
+        Toast.makeText(MainActivity.this, "El destino esta contigo?", Toast.LENGTH_LONG  ).show ();
+    }
+
+    @Override
+    public void onClick (View v){
+
+        switch (v.getId()){
+
+            case R.id.boton:
+                int rand = new Random().nextInt(arrayRespuesta.length);
+                        respuesta.setText(arrayRespuesta[rand]);
+                break;
+        }
     }
 }
